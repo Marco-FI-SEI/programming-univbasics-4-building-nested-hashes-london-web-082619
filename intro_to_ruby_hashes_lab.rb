96769ec23faad84d1e0ff8c9e0f767ec34382577
Marco McNulty <mmc90@hotmail.co.uk>
2019-06-29 10:39:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,9 @@ monopoly = {
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => {}
+	      :reading_railroad => {},
+	      :pennsylvania_railroad => {},
+	      
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
